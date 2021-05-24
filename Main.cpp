ea8c5d970b2112cc94887856e5e38e4f501d0e88
malkootkhan <engr.mkhan1990@gmail.com>
2021-05-24 22:59:58 +0500

    Counting number of characters in the file

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <xstring>
 
 
 using namespace std;
+
+class streng
+{
+	char *str;
+};
 
 int main()
 {
@@ -14,10 +20,14 @@ int main()
 	//Reading file to a string variable
 	ifstream lastFile("MyFile.txt");
 	string textData;
+	int noOfLines= 0;
 	while (getline (lastFile, textData))
 	{
 		//print to console
-		cout << textData;
+		cout << textData<<endl;
+		noOfLines++;
 	}
+	//find the number of characters in the file
+	cout << noOfLines*textData.capacity() << endl;
 	return 0;
 }
